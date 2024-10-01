febb9213d6a0a1c48924ee013a9fe69f221c79ff
Wellen Ramos <rocha.wellen@gmail.com>
2024-09-30 22:33:43 -0300

    Archetype

---
+++ src/main/java/br/com/aroma/aroma_delivery/AromaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.aroma.aroma_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromaDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aroma/aroma_delivery/AromaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aroma.aroma_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AromaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
