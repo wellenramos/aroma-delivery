f7f76afe43f24b9df01a2ae0154416756e7dd280
Wellen Ramos <rocha.wellen@gmail.com>
2024-10-01 00:27:56 -0300

    Configuração da conexão com o banco de dados postgres e configuração do flyway

--- src/main/resources/db/v1__inicial.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE perfil
-(
-    id   SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL
-);
-
-INSERT INTO perfil (nome) VALUES ('admin');
-INSERT INTO perfil (nome) VALUES ('cliente');''
